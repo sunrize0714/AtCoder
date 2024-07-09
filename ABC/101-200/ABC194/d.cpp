f58a6438d7095c6d7b49d8b9000651db44223448
sunrize0714 <133011447+sunrize0714@users.noreply.github.com>
2024-07-10 01:13:23 +0900

    upsolve 194D

---
+++ ABC/101-200/ABC194/d.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+using ll = long long int;
+typedef vector<ll> vll;
+typedef vector<vll> vvll;
+typedef vector<vvll> vvvll;
+typedef vector<bool> vb;
+typedef vector<string> vs;
+typedef pair<ll,ll> pll;
+typedef vector<pair<ll,ll>> vp;
+#define rep(i,a,n) for(ll i=a;i<n;i++)
+#define revrep(i,a,n) for(ll i=n-1;i>=a;i--)
+#define mod 998244353
+#define all(a) a.begin(),a.end()
+
+int main(){
+    ll n;
+    cin >> n;
+    double ans = 0.0;
+    rep(i,1,n){
+        ans += double(n)/double(i);
+    }
+    cout << setprecision(15) << ans << endl;
+}
