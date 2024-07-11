02ad8735e80058606d76d57083191fdd53275995
sunrize0714 <133011447+sunrize0714@users.noreply.github.com>
2024-07-11 13:50:03 +0900

    upsolve 133C

---
+++ ABC/101-200/ABC133/c.cpp
@@ -0,0 +1,60 @@
+#include<bits/stdc++.h>
+using namespace std;
+using ll = long long int;
+typedef vector<ll> vll;
+typedef vector<vll> vvll;
+typedef vector<vvll> vvvll;
+typedef vector<bool> vb;
+typedef vector<string> vs;
+typedef pair<ll,ll> pll;
+typedef vector<pair<ll,ll>> vp;
+#define rep(i,a,n) for(ll i=a;i<n;i++)
+#define revrep(i,a,n) for(ll i=n-1;i>=a;i--)
+#define mod 998244353
+#define all(a) a.begin(),a.end()
+
+bool in_field(ll l,ll r,ll x){
+    if(l <= x && x < r)return true;
+    else return false;
+}
+
+ll distance(ll ax,ll ay,ll bx, ll by){
+    return (ax-bx)*(ax-bx) + (ay-by)*(ay-by);
+}
+
+ll modinv(ll a, ll m) {
+    ll b=m,u=1,v=0;
+    while(b){
+        ll t=a/b;
+        a-=t*b;swap(a,b);
+        u-=t*v;swap(u,v);
+    }
+    u%=m; 
+    if(u<0)u+=m;
+    return u;
+}
+
+bool cross(ll a,ll b,ll l,ll r){
+    return not (b <= l or r <= a);
+}
+
+ll dx4[4] = {1,0,-1,0};
+ll dy4[4] = {0,-1,0,1};
+ll dx8[8] = {1,1,0,-1,-1,-1,0,1};
+ll dy8[8] = {0,-1,-1,-1,0,1,1,1};
+
+int main(){
+    ll l,r;
+    cin >> l >> r;
+    ll ans = 2020;
+    rep(i,l,r+1){
+        rep(j,i+1,r+1){
+            ans = min(ans,i*j%2019);
+            if(ans == 0){
+                cout << ans << endl;
+                return 0;
+            }
+        }
+    }
+    cout << ans << endl;
+}
