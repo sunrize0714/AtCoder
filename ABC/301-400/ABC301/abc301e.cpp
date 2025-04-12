fb0f1532a2d9c7e751509bb0d1bdf1e7815a0812
sunrize0714 <dreamfes2023kasumi@outlook.jp>
2025-04-12 19:50:36 +0900

    BitDP

--- ABC/301-400/ABC301/abc301e.cpp
+++ ABC/301-400/ABC301/abc301e.cpp
@@ -1,74 +1,154 @@
 #include<bits/stdc++.h>
 using namespace std;
-int h,w,t,now=0,start,goal;
-vector<int> g[100000];
-string a[310];
-int okasi[20],dist[100000]={-1};
-queue<int> q;
+using ll = long long int;
+typedef vector<ll> vll;
+typedef vector<vll> vvll;
+typedef vector<vvll> vvvll;
+typedef vector<bool> vb;
+typedef vector<string> vs;
+typedef pair<ll,ll> pll;
+typedef vector<pair<ll,ll>> vp;
+#define rep(i,a,n) for(ll i=a;i<n;i++)
+#define revrep(i,a,n) for(ll i=n-1;i>=a;i--)
+#define mod 998244353
+#define all(a) a.begin(),a.end()
+
+bool in_field(ll l,ll r,ll x){
+    if(l <= x && x < r)return true;
+    else return false;
+}
+
+ll distance(ll ax,ll ay,ll bx, ll by){
+    return (ax-bx)*(ax-bx) + (ay-by)*(ay-by);
+}
+
+ll modinv(ll a, ll m) {
+    ll b=m,u=1,v=0;
+    while(b){
+        ll t=a/b;
+        a-=t*b;swap(a,b);
+        u-=t*v;swap(u,v);
+    }
+    u%=m; 
+    if(u<0)u+=m;
+    return u;
+}
+
+ll dx4[4] = {1,0,-1,0};
+ll dy4[4] = {0,-1,0,1};
+ll dx8[8] = {1,1,0,-1,-1,-1,0,1};
+ll dy8[8] = {0,-1,-1,-1,0,1,1,1};
+
 int main(){
+    ll h,w,t;
     cin >> h >> w >> t;
-    for(int i=0;i<h;i++){
-        cin >> a[i];
-        for(int j=0;j<w;j++){
-            if(a[i][j]=='S')start = (i-1)*w+j;
-            if(a[i][j]=='G')goal = (i-1)*w+j;
-            if(a[i][j]=='o'){
-                okasi[now] =(i-1)*w+j;
-                now++;
+    vs a(h);
+    rep(i,0,h)cin >> a[i];
+    vll ame;
+    ll sx,sy,gx,gy;
+    rep(i,0,h){
+        rep(j,0,w){
+            if(a[i][j] == 'S'){
+                sx = i,sy = j;
+            }
+            if(a[i][j] == 'G'){
+                gx = i,gy = j;
+            }
+            if(a[i][j] == 'o'){
+                ame.push_back(i*w+j);
             }
         }
     }
+    ll cnt = ame.size();
 
-    for(int i=0;i<h;i++){
-        for(int j=0;j<w-1;j++){
-            if(a[i][j]!='#' && a[i][j+1]!='#'){
-                g[(i-1)*w+j].push_back((i-1)*w+j+1);
-                g[(i-1)*w+j+1].push_back((i-1)*w+j);
+    vvll ame_to_ame(cnt,vll(cnt,1e18));
+    rep(i,0,cnt){
+        vll dist(h*w,1e18);
+        dist[ame[i]] = 0;
+        queue<ll> q;
+        q.push(ame[i]);
+        while(!q.empty()){
+            ll x = q.front()/w, y = q.front()%w;
+            q.pop();
+            rep(j,0,4){
+                ll xx = x + dx4[j],yy = y + dy4[j];
+                if(in_field(0,h,xx) && in_field(0,w,yy)){
+                    if(a[xx][yy] != '#' && dist[xx*w+yy] > dist[x*w+y]+1){
+                        q.push(xx*w+yy);
+                        dist[xx*w+yy] = dist[x*w+y] + 1;
+                    }
+                }
             }
+        }
+        rep(j,0,cnt){
+            ame_to_ame[i][j] = ame_to_ame[j][i] = min(ame_to_ame[i][j],dist[ame[j]]);
         }
     }
 
-    for(int i=0;i<h-1;i++){
-        for(int j=0;j<w;j++){
-            if(a[i][j]!='#' && a[i+1][j]!='#'){
-                g[(i-1)*w+j].push_back(i*w+j);
-                g[i*w+j].push_back((i-1)*w+j);
-            }
-        }
-    }
-
-    q.push(start);
-    dist[start]=0;
+    vll pre(cnt,1e18),suf(cnt,1e18);
+    vll dist(h*w,1e18);
+    dist[sx*w+sy] = 0;
+    queue<ll> q;
+    q.push(sx*w+sy);
     while(!q.empty()){
-        int pos = q.front();
+        ll x = q.front()/w, y = q.front()%w;
         q.pop();
-        for(int i=0;i<g[pos].size();i++){
-            int to = g[pos][i];
-            if(dist[to] == -1){
-                dist[to] = dist[pos] + 1;
-                q.push(to);
+        rep(j,0,4){
+            ll xx = x + dx4[j],yy = y + dy4[j];
+            if(in_field(0,h,xx) && in_field(0,w,yy)){
+                if(a[xx][yy] != '#' && dist[xx*w+yy] > dist[x*w+y]+1){
+                    q.push(xx*w+yy);
+                    dist[xx*w+yy] = dist[x*w+y] + 1;
+                }
             }
         }
     }
+    rep(i,0,cnt)pre[i] = dist[ame[i]];
 
-    if(t < dist[goal]){
-        cout << -1 << endl;
+    if(dist[gx*w+gy] > t){
+        cout << -1;
         return 0;
     }
-    int point=0,standing=goal;
-    for(int i=dist[goal];i>0;i--){
-        for(int j=0;j<g[standing].size();j++){
-            if(dist[g[standing][j]] = i-1){
-                standing = g[standing][j];
-                if(a[standing/w])
+
+   
+    rep(i,0,h*w)dist[i] = 1e18;
+    dist[gx*w+gy] = 0;
+    q.push(gx*w+gy);
+    while(!q.empty()){
+        ll x = q.front()/w, y = q.front()%w;
+        q.pop();
+        rep(j,0,4){
+            ll xx = x + dx4[j],yy = y + dy4[j];
+            if(in_field(0,h,xx) && in_field(0,w,yy)){
+                if(a[xx][yy] != '#' && dist[xx*w+yy] > dist[x*w+y]+1){
+                    q.push(xx*w+yy);
+                    dist[xx*w+yy] = dist[x*w+y] + 1;
+                }
             }
         }
     }
+    rep(i,0,cnt)suf[i] = dist[ame[i]];
 
-    for(int i=0;i<now;i++){
-        okasi[i]=min(dist[okasi[i]],abs(dist[goal]-dist[okasi[i]]));
+    vvll dp(1<<cnt,vll(cnt,1e9));
+    rep(i,0,cnt)dp[1<<i][i] = pre[i];
+    rep(i,0,1<<cnt){
+        rep(j,0,cnt){
+            if(dp[i][j] == 1e9)continue;
+            rep(k,0,cnt){
+                if(i & 1<<k)continue;
+                if(dp[i | 1<<k][k] > dp[i][j] + ame_to_ame[j][k])dp[i | 1<<k][k] = dp[i][j] + ame_to_ame[j][k];
+            }
+        }
     }
-    sort(okasi,okasi+now);
-    int left = t-dist[goal];
-    for( ){}
+    ll ans = 0;
+    rep(i,1,1<<cnt){
+        rep(j,0,cnt){
+            if(dp[i][j] + suf[j] <= t){
+                ll pos = 0;
+                rep(k,0,cnt)if(i & 1<<k)pos++;
+                ans = max(ans,pos);
+            }
+        }
+    }
+    cout << ans;
 }
---
+++ ABC/301-400/ABC399/c.cpp
@@ -0,0 +1,102 @@
+#include<bits/stdc++.h>
+using namespace std;
+using ll = long long int;
+typedef vector<ll> vll;
+typedef vector<vll> vvll;
+typedef vector<vvll> vvvll;
+typedef vector<bool> vb;
+typedef vector<string> vs;
+typedef pair<ll,ll> pll;
+typedef vector<pair<ll,ll>> vp;
+#define rep(i,a,n) for(ll i=a;i<n;i++)
+#define revrep(i,a,n) for(ll i=n-1;i>=a;i--)
+#define mod 998244353
+#define all(a) a.begin(),a.end()
+
+bool in_field(ll l,ll r,ll x){
+    if(l <= x && x < r)return true;
+    else return false;
+}
+
+ll distance(ll ax,ll ay,ll bx, ll by){
+    return (ax-bx)*(ax-bx) + (ay-by)*(ay-by);
+}
+
+ll modinv(ll a, ll m) {
+    ll b=m,u=1,v=0;
+    while(b){
+        ll t=a/b;
+        a-=t*b;swap(a,b);
+        u-=t*v;swap(u,v);
+    }
+    u%=m; 
+    if(u<0)u+=m;
+    return u;
+}
+
+ll dx4[4] = {1,0,-1,0};
+ll dy4[4] = {0,-1,0,1};
+ll dx8[8] = {1,1,0,-1,-1,-1,0,1};
+ll dy8[8] = {0,-1,-1,-1,0,1,1,1};
+
+class UnionFind{
+    public:
+    // parent , size 
+    vll par,siz;
+    
+    // make unionfind tree, 0-indexed
+    void init(ll N){
+        par.resize(N,-1);
+        siz.resize(N,1);
+    }
+
+    // return root of x
+    ll root(ll x){
+        while(true){
+            if(par[x] == -1)break;
+            x = par[x];
+        }
+        return x;
+    }
+
+    // unite v and u, update tree and size
+    void unite(ll u,ll v){
+        ll RootU = root(u), RootV = root(v);
+        if(RootU == RootV)return;
+        if(siz[RootU] < siz[RootV]){
+            par[RootU] = RootV;
+            siz[RootV] += siz[RootU];
+        }
+        else{
+            par[RootV] = RootU;
+            siz[RootU] += siz[RootV];
+        }
+    }
+
+    bool same(ll u,ll v){
+        if(root(u) == root(v))return true;
+        return false;
+    }  
+    
+    ll size(ll x){
+        ll pos = root(x);
+        return siz[pos];
+    }
+
+};
+
+int main(){
+    ll n,m;
+    cin >> n >> m;
+    UnionFind UF;
+    UF.init(n);
+    rep(i,0,m){
+        ll u,v;
+        cin >> u >> v;
+        u--,v--;
+        UF.unite(u,v);
+    }
+    set<ll> st;
+    rep(i,0,n)st.insert(UF.root(i));
+    cout << m - (n-st.size());
+}
